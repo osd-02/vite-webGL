a3de281bc5d75bfbd2acc56180ee2bbf1c823128
osd-02 <sakanaq0223@gmail.com>
2021-10-20 03:10:18 +0900

    スタイルの変更

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="fixed bg-opacity-0 m-[10px] tracking-[5px]">
-    <h2 class="text-7xl font-bold">three.js works</h2>
+    <h2 class="text-7xl font-bold underline"><nuxt-link to="/">three.js works</nuxt-link></h2>
     <ul class="m-[10px]">
       <li><nuxt-link to="/works/particles">#1 webGL-demo</nuxt-link></li>
-      <li>#2 webGL-demo</li>
+      <!-- <li>#2 webGL-demo</li>
       <li>#3 webGL-demo</li>
       <li>#4 webGL-demo</li>
       <li>#5 webGL-demo</li>
       <li>#6 webGL-demo</li>
       <li>#7 webGL-demo</li>
       <li>#8 webGL-demo</li>
-      <li>#9 webGL-demo</li>
+      <li>#9 webGL-demo</li> -->
     </ul>
   </div>
 </template>
